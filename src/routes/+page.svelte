fc458a49be35034b9d678990521ed630f24de5d3
irwan75 <irwan.ardyansah75@gmail.com>
2024-11-11 15:22:13 +0700

    add download pdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,19 @@
 	function navigateToExternalApp() {
 		window.location.href = "https://www.google.com";
 	}
+
+	function downloadPDF() {
+		fetch("https://assets.website-files.com/603d0d2db8ec32ba7d44fffe/603d0e327eb2748c8ab1053f_loremipsum.pdf")
+			.then(response => response.blob())
+			.then(blob => {
+				const link = document.createElement('a');
+				link.href = URL.createObjectURL(blob);
+				link.download = 'file.pdf';
+				link.click();
+				URL.revokeObjectURL(link.href);
+			})
+			.catch(error => console.error('Error downloading the PDF:', error));
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -17,4 +30,7 @@
 
 <div style="height: 50px;">
 	<button on:click={navigateToExternalApp}>Navigate to open external app</button>
+</div>
+<div style="height: 50px;">
+	<button on:click={downloadPDF}>Download PDF</button>
 </div>
