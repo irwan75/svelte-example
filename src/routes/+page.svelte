8e9e29c15dce43b61424ba9373cc31b0b3d87dbf
irwan75 <irwan.ardyansah75@gmail.com>
2024-11-11 15:12:38 +0700

    add button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+	function copyText() {
+		navigator.clipboard.writeText("Hello, world!");
+		alert("Copied to clipboard");
+	}
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div style="height: 50px;">
+	<button on:click={copyText}>Copy Text</button>
+</div>
